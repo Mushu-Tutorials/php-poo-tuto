c342906e70f5d60d8d7069f869be814bf3a741c8
MushuLeDragon <dragon.mushu@hotmail.com>
2018-05-02 18:04:06 +0200

    Add healing function

--- Personnage.php
+++ Personnage.php
@@ -5,4 +5,8 @@ class Personnage {
   public $vie = 100;
   public $atk = 20;
 
+  public function Regenerer() {
+    this -> vie += 20;
+  }
+
 }
--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 require 'Personnage.php';
 
+// Initialisation d'un nouveau personnage : Merlin
 $merlin = new Personnage();
 var_dump($merlin);
+// Affichage d'un des paramètres : la vie
+var_dump($merlin -> vie);
 
-
+// Initialisation d'un nouveau personnage : Harry
 $harry = new Personnage();
 var_dump($harry);
